707b730d96a4b0cbf453a2248b6774ef9d93bf0a
KinoshitaYstr <photo_18@outlook.jp>
2020-12-24 14:23:40 +0900

    update

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,63 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+    // no key
+    @0
+    D=A
+    @0
+    M=D // reg0 <- color0
+    // key status
+    @24576
+    D=M
+    @SCREEN
+    D; JEQ
+
+    // yes key
+    @1
+    D=A
+    @0
+    M=D // reg0 <- color1
+    // key status
+    @24576
+    D=M
+    @SCREEN
+    D; JNE
+
+    @LOOP
+    0; JMP
+
+(SCREEN)
+    @99999999
+    D=A
+    @16384
+    D=D+A
+    @1
+    M=D // max pos (reg1)
+    
+    @16384 // start screen address
+    D=A
+    @2
+    M=D // set now screen pos
+
+(SCREENLOOP)
+    @0
+    D=M // get color
+    @2
+    A=M // get and set pos
+    M=D // update color
+
+    D=A+1 
+    @2
+    M=D // update screen pos
+
+    @1
+    D=M
+    @2
+    D=D-M // check pos
+    @SCREENLOOP
+    D; JNE
+
+    @LOOP
+    0; JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+// R0*R1 -> R2
+
+    // init
+    @0
+    D=A // data register is 0
+    @2
+    M=D // reg2 <- data register(0)
+
+(LOOP)
+    // check
+    @1
+    M=M-1  // reg1 <- reg1-1
+    D=M
+    @END
+    D; JLT // if data register < 0
+
+    // add
+    @0
+    D=M    // data register <- reg0
+    @2
+    M=M+D  // reg2 <- reg2+reg0
+    
+    @LOOP
+    0; JMP
+(END)
